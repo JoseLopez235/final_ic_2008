37bf45d8d2a122c9be407114da926a3dbb1e9fcc
Jose Lopez <joselopez11394@gmail.com>
2020-09-23 11:20:30 -0400

    adds methods for restock and stock check

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -3,4 +3,20 @@ class Pantry
   def initialize
     @stock = {}
   end
+
+  def restock(ingredient, amount)
+    if @stock[ingredient].nil?
+      @stock[ingredient] = amount
+    else
+      @stock[ingredient] += amount
+    end
+  end
+
+  def stock_check(ingredient)
+    if @stock[ingredient].nil?
+      0
+    else
+      @stock[ingredient]
+    end
+  end
 end
