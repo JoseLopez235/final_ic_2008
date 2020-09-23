eaac3c6711031219572c5add12ea1e8f06a88354
Jose Lopez <joselopez11394@gmail.com>
2020-09-23 11:30:37 -0400

    adds test for adding ingredients and the method ingredients

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -14,4 +14,29 @@ class RecipeTest < Minitest::Test
     assert_equal "Mac and Cheese", recipe1.name
     assert_equal ({}), recipe1.ingredients_required
   end
+
+  def test_should_add_ingredients_to_ingredients_required
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+    expected = {
+      ingredient1 => 6,
+      ingredient2 => 8
+    }
+    assert_equal expected, recipe1.ingredients_required
+  end
+
+  def test_should_return_array_of_all_ingredients
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+    expected = [ingredient1, ingredient2]
+    assert_equal expected, recipe1.ingredients
+  end
 end
