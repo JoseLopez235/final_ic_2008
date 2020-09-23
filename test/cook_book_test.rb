32845cdf0fa9bdfd3beeebe3476b0840fd345e93
Jose Lopez <joselopez11394@gmail.com>
2020-09-23 11:53:04 -0400

    adds test for ingredients in cookbook and fixed syntax issues

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -1,20 +1,38 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/cook_book"
-require "./lib/cook_book"
+require "./lib/ingredient"
 require "./lib/recipe"
+
 class CookBookTest < Minitest::Test
   def test_should_exist
     cookbook = CookBook.new
     assert_instance_of CookBook, cookbook
   end
 
-  def test_should_be_ablie_to_add_recipes
+  def test_should_be_able_to_add_recipes
     cookbook = CookBook.new
     recipe1 = Recipe.new("Mac and Cheese")
     recipe2 = Recipe.new("Cheese Burger")
     cookbook.add_recipe(recipe1)
     cookbook.add_recipe(recipe2)
     assert_equal [recipe1, recipe2], cookbook.recipes
+  end
+
+  def test_should_return_array_of_all_ingredients_per_recipe
+    cookbook = CookBook.new
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe2 = Recipe.new("Cheese Burger")
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+    expected = ["Cheese", "Macaroni", "Ground Beef", "Bun"]
+    assert_equal expected, cookbook.ingredients
   end
 end
