42d11a40101345dc1ce5259e1853adaa881a515d
Jose Lopez <joselopez11394@gmail.com>
2020-09-23 12:04:57 -0400

    adds method for highest calorie meal

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -17,4 +17,16 @@ class CookBook
     end
     all_ingredients.uniq
   end
+
+  def highest_calorie_meal
+    highest_calories = 0
+    highest_calorie_recipe = nil
+    @recipes.each do |recipe|
+      if recipe.total_calories > highest_calories
+        highest_calories += recipe.total_calories
+        highest_calorie_recipe = recipe
+      end
+    end
+    highest_calorie_recipe
+  end
 end
